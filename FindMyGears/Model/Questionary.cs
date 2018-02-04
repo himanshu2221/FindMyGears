18732db1e656f5baf71a4368568268f5d47e4f94
Pooja Jain <jainpooja12221@gmail.com>
2018-02-04 16:09:37 +0530

    Updating version1.1

--- FindMyGears/Model/Questionary.cs
+++ FindMyGears/Model/Questionary.cs
@@ -8,8 +8,11 @@ namespace FindMyGears.Model
     [Serializable]
     public class Questionary
     {
-        public string Question { get; set; }
-        public string Answer { get; set; }
-        public int Id { get; set; }
+        public const string nameQuestion = "May I know your Age?";
+        public const string ageQuestion = "What is your Weight?";
+        public const string genderQuestion = "May I know your Gender please?";
+        public const string sizeQuestion = "What is your Shoes size?";
+        public const string runningTypeQuestion = "";
+
     }
 }
