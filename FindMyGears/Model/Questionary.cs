577abd19e60a4e281b35864a3243db5252a379da
Pooja Jain <jainpooja12221@gmail.com>
2018-02-04 15:56:12 +0530

    Updating Version 1

---
+++ FindMyGears/Model/Questionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyGears.Model
+{
+    [Serializable]
+    public class Questionary
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int Id { get; set; }
+    }
+}
